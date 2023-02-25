129436fc431dc482f0a662a79dcc14448796572a
Thabish <kadertabish2@gmail.com>
2023-02-25 12:47:55 +0400

    joke-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- r3f-intro/src/components/Overlay.tsx
+++ r3f-intro/src/components/Overlay.tsx
@@ -3,11 +3,6 @@ import { ColorPicker } from "./ColorPicker";
 import PopUp from "./Popup";
 
 export const Overlay = () => {
-	const handleBuy = () => {
-		alert(
-			"Congratulations to me on buying a robot dog! I can now proudly say that I have a pet that won't judge me for eating a whole pizza by myself on a Friday night. And if anyone tells me that having a robot dog is a sign of loneliness, I'll just tell them it's better than talking to my houseplants. At least the robot dog will respond with more than just a wilted leaf."
-		);
-	};
 	return (
 		<section className="absolute top-[30%] left-32 z-10 hidden lg:block">
 			<h1 className="text-5xl font-bold text-black tracking-widest">
@@ -24,12 +19,6 @@ export const Overlay = () => {
 			</h1>
 			<ColorPicker dronePart={"Outer"} />
 			<ColorPicker dronePart={"Skeleton"} />
-			{/* <button
-				onClick={handleBuy}
-				className="p-2 border rounded-lg w-full mt-4 hover:bg-black hover:text-white transition-colors duration-300"
-			>
-				Buy Now
-			</button> */}
 			<PopUp />
 		</section>
 	);
