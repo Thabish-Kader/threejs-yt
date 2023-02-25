da236d09dd04f6937544fb9c8816e4eb2daaef46
Thabish <kadertabish2@gmail.com>
2023-02-25 12:46:23 +0400

    joke-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- r3f-intro/src/components/Popup.tsx
+++ r3f-intro/src/components/Popup.tsx
@@ -2,7 +2,7 @@ import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 
 export default function PopUp() {
-	let [isOpen, setIsOpen] = useState(true);
+	let [isOpen, setIsOpen] = useState(false);
 
 	function closeModal() {
 		setIsOpen(false);
