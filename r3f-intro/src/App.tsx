23abb3d0cbd296cd11690b4d5b2340ffe796e034
Thabish <kadertabish2@gmail.com>
2023-02-26 13:25:19 +0400

    read-me
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- r3f-intro/src/App.tsx
+++ r3f-intro/src/App.tsx
@@ -4,12 +4,6 @@ import { MyCanvas } from "./components/MyCanvas";
 function App() {
 	return (
 		<div className="h-screen ">
-			{/* <div className="grid grid-cols-3 h-screen hero-section">
-				<Info />
-				<div className="col-span-3 lg:col-span-2 ">
-					<MyCanvas />
-				</div>
-			</div> */}
 			<Overlay />
 			<MyCanvas />
 		</div>
